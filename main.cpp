195b9d41b572f9d1cfd16de77a2e729ca3c6c22d
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-05-02 16:30:33 -0400

    Changed super loop for CLI arguments

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#define INPUT_BUFFER_SIZE 40 // The maximum length of a GH username is 39 characters, plus EOS character
-int main() {
-    char input[INPUT_BUFFER_SIZE];
-    while (true) { // Main super loop for the program
-        printf("github-user-activity> ");
-        fgets(input, sizeof(input), stdin);
-        size_t len = strlen(input);
-        if ((len!=0) && (input[len - 1] == '\n')) //Trims the new line characters off of the input (prevents double new line)
-            input[len - 1] = '\0';
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: github-activity <username>\n";
+        return 1;
     }
     return 0;
 }
